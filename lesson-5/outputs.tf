67846b4006e93956b1fc16e347a8d915de6d8133
Artur Didur <didur.art@gmail.com>
2025-07-17 21:40:50 +0200

    Clean commit without .terraform and lockfile

---
+++ lesson-5/outputs.tf
@@ -0,0 +1,24 @@
+output "s3_bucket_name" {
+  description = "Назва S3-бакета для стейтів"
+  value       = module.s3_backend.s3_bucket_name
+}
+
+output "dynamodb_table_name" {
+  description = "Назва таблиці DynamoDB для блокування стейтів"
+  value       = module.s3_backend.dynamodb_table_name
+}
+
+output "ecr_repository_url" {
+  description = "URL ECR репозиторію"
+  value       = module.ecr.ecr_repository_url
+}
+
+output "ecr_repository_name" {
+  description = "Назва ECR репозиторію"
+  value       = module.ecr.ecr_repository_name
+}
+
+output "ecr_repository_arn" {
+  description = "ARN ECR репозиторію"
+  value       = module.ecr.ecr_repository_arn
+}
