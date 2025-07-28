ab66c996c485b9f70bf2c45e9857b42caf9807cb
Artur Didur <didur.art@gmail.com>
2025-07-28 21:06:03 +0200

    fix dockerfile

--- lesson-8-9/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "s3-bucket-ja" # <-- Поміняй тут на імʼя створеного бакету
-    key            = "lesson-8-9/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
