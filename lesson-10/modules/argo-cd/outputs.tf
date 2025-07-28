60de04fee2fba727ce288f1b6e4161622a40b8eb
Artur Didur <didur.art@gmail.com>
2025-07-28 13:22:45 +0200

    lesson-10

--- lesson-10/modules/argo-cd/outputs.tf
+++ lesson-10/modules/argo-cd/outputs.tf
@@ -3,7 +3,15 @@ output "argo_cd_server_service" {
   value       = "argo-cd.${var.namespace}.svc.cluster.local"
 }
 
-output "admin_password" {
+output "argo_cd_admin_password" {
   description = "Initial admin password"
   value       = "Run: kubectl -n ${var.namespace} get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d"
+}
+
+output "argo_cd_namespace" {
+  value = var.namespace
+}
+
+output "argo_apps_release_name" {
+  value = helm_release.argo_apps.name
 }
